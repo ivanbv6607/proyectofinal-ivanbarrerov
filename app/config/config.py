3527b129bd00cd0252ccbc11dc76dfbaa35c07aa
ivanb6607 <95305522+ivanb6607@users.noreply.github.com>
2025-04-01 11:54:54 -0500

    Abril 1 - 5

--- app/config/config.py
+++ app/config/config.py
@@ -5,7 +5,7 @@ import psycopg2
 load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI','postgresql://postgres:4a7DinSowBtXP6kg@db.beuliblewzcpsfbzhmls.supabase.co:5432/postgres')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI','postgresql://postgres:4a7DinSowBtXP6kg@db.beuliblewzcpsfbzhmls.supabase.co:6543/postgres')
                                         
     #                                    'postgresql://default.lolamzuokwevbtjjfjsr:4a7DinSowBtXP6kg@aws-0-us-east-1.pooler.supabase.com:6543/heladerias_db')
     SECRET_KEY = os.urandom(24)
